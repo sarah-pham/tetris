e336b7578db5cda3dc158216f40158b27cf24984
Sarah Pham <sarahdzung@gmail.com>
2024-02-05 23:30:45 +1100

    Rename constant variables and add comment descriptions

--- game/gui.py
+++ game/gui.py
@@ -4,32 +4,32 @@ import config
 class GUI:
     def __init__(self, screen):
         self.screen = screen
-        self.play_grid = pygame.Surface((config.BOARD_WIDTH, config.BOARD_HEIGHT))
-        self.play_grid_center = ((config.SCREEN_WIDTH-config.BOARD_WIDTH)/2, 
-                                 (config.SCREEN_HEIGHT-config.BOARD_HEIGHT)/2)
+        self.play_grid = pygame.Surface((config.GRID_WIDTH, config.GRID_HEIGHT))
+        self.play_grid_center = ((config.SCREEN_WIDTH-config.GRID_WIDTH)/2,
+                                 (config.SCREEN_HEIGHT-config.GRID_HEIGHT)/2)
 
     def draw_grid(self):
         # Draw vertical grid lines
         self.play_grid.fill(config.GRID_BACKGROUND_COLOR)
         for i in range(config.GRID_COLS + 1):
             x = i * config.BLOCK_SIZE
-            if x == config.BOARD_WIDTH:
+            if x == config.GRID_WIDTH:
                 x -= 1 # Ensure that line is drawn within surface boundaries
             pygame.draw.line(self.play_grid,
                              color=config.GRID_LINE_COLOR,
                              start_pos=(x, 0),
-                             end_pos=(x, config.BOARD_HEIGHT),
+                             end_pos=(x, config.GRID_HEIGHT),
                              width=config.GRID_LINE_WIDTH)
 
          # Draw horizontal grid lines
         for j in range(config.GRID_ROWS + 1):
             y = j * config.BLOCK_SIZE
-            if y == config.BOARD_HEIGHT:
+            if y == config.GRID_HEIGHT:
                 y -= 1 # Ensure that line is drawn within surface boundaries
             pygame.draw.line(self.play_grid,
                              color=config.GRID_LINE_COLOR,
                              start_pos=(0, y),
-                             end_pos=(config.BOARD_WIDTH, y),
+                             end_pos=(config.GRID_WIDTH, y),
                              width=config.GRID_LINE_WIDTH)
 
         self.screen.blit(self.play_grid, self.play_grid_center)

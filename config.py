74e08ae9edaef6fd66fcb8719dc0c3818bda51f6
Sarah Pham <sarahdzung@gmail.com>
2024-02-06 17:48:22 +1100

    Update comments in config.py

--- config.py
+++ config.py
@@ -2,17 +2,17 @@
 GRID_ROWS = 20      # Number of rows in the Tetris grid
 GRID_COLS = 10      # Number of columns in the Tetris grid
 
-BLOCK_SIZE = 30     # Size of each block within the grid (in pixels)
-GRID_LINE_WIDTH = 1 # Width of the grid lines (in pixels)
+BLOCK_SIZE = 30         # Size of each block within the grid (pixels)
+GRID_LINE_WIDTH = 1     # Width of the grid lines (in pixels)
 
 # Calculated grid size
-GRID_WIDTH = GRID_COLS * BLOCK_SIZE     # Total width of grid
-GRID_HEIGHT = GRID_ROWS * BLOCK_SIZE    # Total height of grid
+GRID_WIDTH = GRID_COLS * BLOCK_SIZE     # Total width of grid (pixels)
+GRID_HEIGHT = GRID_ROWS * BLOCK_SIZE    # Total height of grid (pixels)
 
 # Grid colors
-GRID_BACKGROUND_COLOR = (0, 0, 0)
-GRID_LINE_COLOR = (36, 36, 36)
+GRID_BACKGROUND_COLOR = (0, 0, 0)   # Color of the Tetris grid background
+GRID_LINE_COLOR = (36, 36, 36)      # Color of the grid lines
 
 # Display settings
-SCREEN_WIDTH = 800  # Width of the entire game screen (in pixels)
-SCREEN_HEIGHT = 600 # Height of the entire game screen (in pixels)
+SCREEN_WIDTH = 800  # Width of the entire game screen (pixels)
+SCREEN_HEIGHT = 600 # Height of the entire game screen (pixels)

8262518bbfca161ca164953e6b87c54e8afb1019
Sarah Pham <sarahdzung@gmail.com>
2024-02-06 16:37:05 +1100

    Refactor grid variable names

--- game/gui.py
+++ game/gui.py
@@ -4,18 +4,18 @@ import config
 class GUI:
     def __init__(self, screen):
         self.screen = screen
-        self.play_grid = pygame.Surface((config.GRID_WIDTH, config.GRID_HEIGHT))
-        self.play_grid_center = ((config.SCREEN_WIDTH-config.GRID_WIDTH)/2,
+        self.grid = pygame.Surface((config.GRID_WIDTH, config.GRID_HEIGHT))
+        self.grid_center = ((config.SCREEN_WIDTH-config.GRID_WIDTH)/2,
                                  (config.SCREEN_HEIGHT-config.GRID_HEIGHT)/2)
 
     def draw_grid(self):
         # Draw vertical grid lines
-        self.play_grid.fill(config.GRID_BACKGROUND_COLOR)
+        self.grid.fill(config.GRID_BACKGROUND_COLOR)
         for i in range(config.GRID_COLS + 1):
             x = i * config.BLOCK_SIZE
             if x == config.GRID_WIDTH:
                 x -= 1 # Ensure that line is drawn within surface boundaries
-            pygame.draw.line(self.play_grid,
+            pygame.draw.line(self.grid,
                              color=config.GRID_LINE_COLOR,
                              start_pos=(x, 0),
                              end_pos=(x, config.GRID_HEIGHT),
@@ -26,10 +26,10 @@ class GUI:
             y = j * config.BLOCK_SIZE
             if y == config.GRID_HEIGHT:
                 y -= 1 # Ensure that line is drawn within surface boundaries
-            pygame.draw.line(self.play_grid,
+            pygame.draw.line(self.grid,
                              color=config.GRID_LINE_COLOR,
                              start_pos=(0, y),
                              end_pos=(config.GRID_WIDTH, y),
                              width=config.GRID_LINE_WIDTH)
 
-        self.screen.blit(self.play_grid, self.play_grid_center)
+        self.screen.blit(self.grid, self.grid_center)
